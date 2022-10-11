8fb4fd3f86e95b975de4b2d3ea93d134deba106e
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-10-11 18:18:53 -0300

    criado estrutura inicial da pagina de login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,42 @@
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+import './home.css'
+
+
 export default function Home(){
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    
+    function handleLogin(e) {
+       e.preventDefault()
+       if(email !== '' && password !== ''){
+            alert("Campos Prenchidos...")
+       }else{
+            alert("Preencha todos os Campos.")
+       }
+    }
     return(
-        <div>
-            <h1>Pagina Home</h1>
-        </div>
+     <div className='home-container'>
+        <h1>Lista de Tarefas</h1>
+        <span>Gerencie sua agenda de forma fácil</span>
+        <form className='form' onSubmit={handleLogin}>
+            <input type="text"
+            placeholder='Digite seu email...' 
+                value={email}
+                onChange={ (e) => setEmail(e.target.value)}
+            />
+            
+            <input type="password"
+            autoComplete={false}
+            placeholder='*********' 
+                value={password}
+                onChange={ (e) => setPassword(e.target.value)}
+            />
+            
+            <button type='submit'>Acessar</button>
+        </form>
+        
+        <Link to="/register" className='button-link'>Não possui conta? Cadastre-se</Link>
+     </div>
     )
 }
